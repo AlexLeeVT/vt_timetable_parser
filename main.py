2258ea41379871e5dde66c35717bda2785202559
AlexLeeVT <95508161+AlexLeeVT@users.noreply.github.com>
2026-01-22 02:15:04 -0500

    working except last row is not included

--- main.py
+++ main.py
@@ -40,6 +40,10 @@ data_keys = ['crn', 'course', 'title', 'schedule_type', 'modality',
              'days', 'begin', 'end', 
              'location', 'exam',]
 
+comment_keys = ['comment']
+
+additional_time_keys = ['ad_days', 'ad_begin', 'ad_end', 'ad_location']
+
 def parse_row(row, additional_time=None, comment=None): 
     """
     input:
@@ -83,12 +87,86 @@ def make_request():
     r = requests.post(url, data=params)
     return BeautifulSoup(r.content, 'html.parser')
 
+def parse_row(row):
+    return [entry.text.replace('\n', '').replace('-', ' ').strip() for entry in row.find_all('td')] 
+
+def has_comment(entries):
+    return "comments" in entries[0].lower()
+
+def has_additionaltimes(entries):
+    if len(entries) <= 4:
+        return False
+    
+    return ("additional times" in entries[4].lower())
+
 # parse table
 def parse_table(table):
     table.find('table', attrs={'class':'dataentrytable'})
-    rows = [row for row in table.find_all('tr') if row.attrs == {}] 
 
-    return [parse_row(c) for c in rows[VTTOC_COURSETABLE_ROW_START : VTTOC_COURSETABLE_ROW_START+10] if parse_row(c) is not None]
+    old_row = None
+    comment = None
+    addit_times = None
+
+    count = 0
+
+    courses = list()
+    # state machine
+    for row in table.find_all('tr'):
+        # filter for valid table rows
+        if row.attrs != {}:
+            continue
+
+        # parse row
+        entries = parse_row(row)
+
+        # filter for valid entries
+        if len(entries) <= 1:
+            continue
+
+        # check if current row is comment
+        if has_comment(entries):
+            comment = [entries[1]]
+
+        # check if current row is additional times
+        elif has_additionaltimes(entries):
+            addit_times = entries[5:9]
+
+            if "arr" in addit_times[1].lower():
+                addit_times[3] = addit_times[2]
+                addit_times[2] = '(ARR)'
+            
+        # submit old row, set old row to current row, reset comment and additional times  
+        else:
+            if old_row:
+                if len(old_row) > 7:
+                    if 'arr' in old_row[8].lower():
+                        old_row.insert(9, old_row[8])
+
+                additional_keys = [] 
+                if comment:
+                    old_row.extend(comment)
+                    additional_keys += comment_keys 
+
+                if addit_times:
+                    old_row.extend(addit_times)
+                    additional_keys += additional_time_keys
+
+                keys = data_keys + additional_keys
+                courses.append(dict(zip(keys, old_row)))
+
+                comment = None
+                addit_times = None
+
+            # hold old row for potential extra notes
+            if len(entries) >= 12:
+                if 'arr' in entries[8].lower():
+                    old_row = entries[:12]
+                else:
+                    old_row = entries[:13]
+            else:
+                old_row = entries
+
+    return courses
 
 def get_TOC_courses():
     html_table = make_request()
@@ -97,7 +175,9 @@ def get_TOC_courses():
     return table
 
 courses = get_TOC_courses()
-print(courses[0])
+for course in courses:
+    print(course)
+
 # output to file
 output = Path("output.txt")
 if not output.exists:
