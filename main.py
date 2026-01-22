e11796297b100c3722edc75a5544e4312dae59d4
AlexLeeVT <95508161+AlexLeeVT@users.noreply.github.com>
2026-01-22 02:27:04 -0500

    data parsing complete. TODO: Convert into dataframe

--- main.py
+++ main.py
@@ -6,35 +6,6 @@ from bs4 import BeautifulSoup
 
 from pathlib import Path
 
-# referenced from https://github.com/VirginiaTech/pyvt
-class Section:
-
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    @staticmethod
-    def tuple_str(tup):
-        return str(tup).replace("'", "")
-
-    def __str__(self):
-        return '%s (%s) on %s at %s' % (getattr(self, 'name'), getattr(self, 'crn'),
-                                        getattr(self, 'days'),
-                                        Section.tuple_str((getattr(self, 'start_time'), getattr(self, 'end_time'))))
-
-    def __eq__(self, other):
-        if isinstance(other, Section):
-            return self.__dict__ == other.__dict__
-        return False
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __repr__(self):
-        return str(self)
-
-    def __hash__(self):
-        return int(getattr(self, 'crn'))
-
 data_keys = ['crn', 'course', 'title', 'schedule_type', 'modality',
              'credits', 'capacity', 'instructor', 
              'days', 'begin', 'end', 
@@ -43,26 +14,6 @@ data_keys = ['crn', 'course', 'title', 'schedule_type', 'modality',
 comment_keys = ['comment']
 
 additional_time_keys = ['ad_days', 'ad_begin', 'ad_end', 'ad_location']
-
-def parse_row(row, additional_time=None, comment=None): 
-    """
-    input:
-        row: list(str)
-        additional_time: list(str) 
-        comment: list(str)
-
-    return:
-        dictionary of courses with the additional time and comment if provided.
-    """
-    entries = [entry.text.replace('\n', '').replace('-', ' ').strip() for entry in row.find_all('td')]
-
-    if len(entries) <= 1:
-        return None
-
-    return dict(zip(data_keys, entries))
-
-# in-between there are many 
-VTTOC_COURSETABLE_ROW_START = 12
 
 # POST request
 params = {
@@ -99,6 +50,23 @@ def has_additionaltimes(entries):
     
     return ("additional times" in entries[4].lower())
 
+def synthesize_data(old_row, comment=None, addit_times=None):
+    if len(old_row) > 7:
+        if 'arr' in old_row[8].lower():
+            old_row.insert(9, old_row[8])
+
+    additional_keys = [] 
+    if comment:
+        old_row.extend(comment)
+        additional_keys += comment_keys 
+
+    if addit_times:
+        old_row.extend(addit_times)
+        additional_keys += additional_time_keys
+
+    keys = data_keys + additional_keys
+    return dict(zip(keys, old_row))
+
 # parse table
 def parse_table(table):
     table.find('table', attrs={'class':'dataentrytable'})
@@ -106,7 +74,7 @@ def parse_table(table):
     old_row = None
     comment = None
     addit_times = None
-
+    
     count = 0
 
     courses = list()
@@ -125,7 +93,8 @@ def parse_table(table):
 
         # check if current row is comment
         if has_comment(entries):
-            comment = [entries[1]]
+            count+=1
+            comment = [f"{entries[0]} {entries[1]}"]
 
         # check if current row is additional times
         elif has_additionaltimes(entries):
@@ -138,21 +107,7 @@ def parse_table(table):
         # submit old row, set old row to current row, reset comment and additional times  
         else:
             if old_row:
-                if len(old_row) > 7:
-                    if 'arr' in old_row[8].lower():
-                        old_row.insert(9, old_row[8])
-
-                additional_keys = [] 
-                if comment:
-                    old_row.extend(comment)
-                    additional_keys += comment_keys 
-
-                if addit_times:
-                    old_row.extend(addit_times)
-                    additional_keys += additional_time_keys
-
-                keys = data_keys + additional_keys
-                courses.append(dict(zip(keys, old_row)))
+                courses.append(synthesize_data(old_row, comment, addit_times))
 
                 comment = None
                 addit_times = None
@@ -166,6 +121,8 @@ def parse_table(table):
             else:
                 old_row = entries
 
+#    print(count)
+    courses.append(synthesize_data(old_row, comment, addit_times))
     return courses
 
 def get_TOC_courses():
@@ -175,8 +132,8 @@ def get_TOC_courses():
     return table
 
 courses = get_TOC_courses()
-for course in courses:
-    print(course)
+print(courses)
+#print(len(courses))
 
 # output to file
 output = Path("output.txt")
